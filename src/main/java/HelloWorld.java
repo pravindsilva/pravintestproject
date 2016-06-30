25cbe77af3a9a700519afde397d7ecb21d074efc
pravin dsilva <pravindsilva@outlook.com>
2016-06-30 17:48:13 +0530

    pravin test

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,9 +6,8 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("asddHELLOW OWL");
-System.out.println("asddHELLOW OWL");
-System.out.println("asddHELLOW OWL");
+System.out.println("This is a calculator");
+
 	}
 
 }
---
+++ src/main/java/RunCalculator.java
@@ -0,0 +1,6 @@
+
+public class RunCalculator {
+	public void runCalculator(int firstNumber, int secondNumber){
+		System.out.println(firstNumber + secondNumber);
+	}
+}
