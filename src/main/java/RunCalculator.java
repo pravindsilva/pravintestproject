7c646cbf30b4cd36f1e3029fdee17e8bb976495e
pravin dsilva <pravindsilva@outlook.com>
2016-07-14 16:03:43 +0530

    first commite test

--- src/main/java/RunCalculator.java
+++ src/main/java/RunCalculator.java
@@ -1,7 +1,8 @@
 
 public class RunCalculator {
 	public int runCalculator(int firstNumber, int secondNumber){
-		System.out.println("this is part of the patch");
+		System.out.println("this is part of the patch and not a big change");
+		System.out.println("testing changes mad");
 		 return firstNumber + secondNumber;
 	}
 }
